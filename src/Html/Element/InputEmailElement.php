0a85d5e03940484a83b1420eea407474cada992d
Dez <dez@dev-id.net>
2015-11-21 18:46:27 +0200

    add input email

---
+++ src/Html/Element/InputEmailElement.php
@@ -0,0 +1,19 @@
+<?php
+
+    namespace Dez\Html\Element;
+
+    class InputEmailElement extends InputElement {
+
+        /**
+         * InputEmailElement constructor.
+         * @param $name
+         * @param null $value
+         * @param array $attributes
+         */
+        public function __construct($name, $value = null, array $attributes = [])
+        {
+            parent::__construct($name, $value, $attributes);
+            $this->setAttribute('type', 'email');
+        }
+
+    }
